91f37a4c6c77d2d4844cb8ae294f8278eacfbdb5
Negolng <114674976+Negolng@users.noreply.github.com>
2023-11-30 17:24:12 +0500

    Update main.py
    Changed keyboard commands and added new physics

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-import pygame
 from random import randint
+from math import sqrt
+import pygame
 
 
 class Ball:
-    def __init__(self, disp, cords, dispsize, fps, rad=15, colour=(255, 0, 0), kk=0.85):
+    def __init__(self, disp, cords, dispsize, fps, other_balls, rad=15, colour=None,
+                 kk=0.8, yvel=0, xvel=0, gravity=9.806):
+        global c
         self.disp = disp
         self.x, self.y = self.cords = cords
         self.rad = rad
         self.colour = colour
-        self.vel = 0
+
+        self.yvel = yvel
+        self.xvel = xvel
+
+        self.balls = other_balls
         self.dispsize = dispsize
         self.fps = fps
         self.kk = kk
+        self.colls = list()
+        self.lx = 0
+        self.ly = 0
+        self.dodraw = True
 
-        self.gravp = 9.806
+        if not colour:
+            self.colour = randint(0, 255), randint(0, 255), randint(0, 255)
+        else:
+            self.colour = colour
+
+        self.gravp = gravity
 
     def draw(self):
-        pygame.draw.circle(surface=self.disp, color=self.colour, center=self.cords, radius=self.rad)
+        if self.dodraw:
+            pygame.draw.circle(surface=self.disp, color=self.colour, center=self.cords, radius=self.rad)
+        else:
+            del self
 
     def render(self):
+        self.other_balls_collide()
         self.change_cords()
         self.upcord()
         self.draw()
@@ -27,24 +47,125 @@ class Ball:
         self.cords = (self.x, self.y)
 
     def bounce(self):
-        self.vel = -(self.vel * self.kk)
+        if abs(self.yvel * self.kk) >= 10:
+            self.yvel = -(self.yvel * self.kk)
+            self.xvel = self.xvel * self.kk
+        else:
+            self.yvel = 0
+            self.xvel = self.xvel * self.kk
+
+    def bounce_wall(self):
+        self.yvel = self.yvel * self.kk
+        self.xvel = -(self.xvel * self.kk)
+
+    def collided(self, cords, er=0):
+        outl = []
+        for nut in self.balls:
+
+            if nut != self:
+
+                ox, oy = nut.cords
+                sx, sy = cords
+
+                xdiff = abs(ox - sx)
+                ydiff = abs(oy - sy)
+
+                distance = sqrt(xdiff**2 + ydiff**2)
+                # print(distance, (self.rad + nut.rad))
+                if distance + er <= (self.rad + nut.rad):
+                    outl.append(nut)
+        self.colls = outl
+
+    def other_balls_collide(self):
+        # grab your left nut, make right one jealous!
+        self.collided(self.cords)
+        collided_balls = self.colls
+        if collided_balls:
+            for ball in collided_balls:
+                if self.lx == 0 and self.ly == 0:
+                    self.dodraw = False
+                    # ox, oy = ball.cords
+                    # sx, sy = self.cords
+                    # xdiff = ox - sx
+                    # ydiff = oy - sy
+
+                olvx = self.xvel
+                olvy = self.yvel
+                self.xvel = -self.xvel * self.kk / 2
+                self.yvel = -self.yvel * self.kk / 2
+
+                ball.xvel += olvx * ball.kk / 2
+                ball.yvel += olvy * ball.kk / 2
+
+                self.x = self.lx
+                self.y = self.ly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def change_cords(self):
+
         if self.y + self.rad <= self.dispsize[1]:
-            self.change_vel()
-            newy = self.y + self.vel
-            if newy + self.rad <= self.dispsize[1]:
-                self.y = newy
-            else:
+            self.gravity()
+
+        else:
+            if self.yvel > 0:
                 self.y = self.dispsize[1] - self.rad
                 self.bounce()
 
-        else:
-            self.y = self.dispsize[1] - self.rad
-            self.bounce()
+        newy = self.y + self.yvel / self.fps
+        newx = self.x + self.xvel / self.fps
 
-    def change_vel(self):
-        self.vel += self.gravp * (1 / self.fps)
+        self.collided((newx, newy))
+
+        if self.xvel != 0 or (self.yvel != 0 or str(self.yvel) != str(self.gravp)):
+
+            if newy + self.rad <= self.dispsize[1] and newy - self.rad > 0:
+                self.ly = self.y
+                self.y = newy
+
+            else:
+                if not newy + self.rad <= self.dispsize[1]:
+                    self.ly = self.y
+                    self.y = self.dispsize[1] - self.rad
+                    self.bounce()
+                elif newy - self.rad <= 0:
+                    self.ly = self.y
+                    self.y = self.rad
+                    self.bounce()
+
+            if newx + self.rad <= self.dispsize[0] and newx - self.rad > 0:
+                self.lx = self.x
+                self.x = newx
+
+            else:
+                if not newx + self.rad <= self.dispsize[0]:
+                    self.lx = self.x
+                    self.x = self.dispsize[0] - self.rad
+                    self.bounce_wall()
+                elif newx - self.rad <= 0:
+                    self.lx = self.x
+                    self.x = self.rad
+                    self.bounce_wall()
+
+    def gravity(self):
+        self.yvel += self.gravp
 
 
 if __name__ == "__main__":
@@ -55,28 +176,61 @@ if __name__ == "__main__":
 
     run = True
 
-    frps = 75
-
-    circle = Ball(mscr, (300, 300), size, frps)
+    frps = 300
 
     clock = pygame.time.Clock()
 
     balls = []
+
+    power = 3
 
     while run:
         mscr.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                color = (randint(0, 255), randint(0, 255), randint(0, 255))
-                balls.append(Ball(mscr, event.pos, size, frps, colour=color))
 
             if event.type == pygame.KEYDOWN:
                 keys = pygame.key.get_pressed()
+                # print(keys)
                 if keys[pygame.K_SPACE]:
                     balls = []
-                    print('cleared')
+
+                elif keys[pygame.K_LEFT]:
+                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, xvel=-250 * power))
+
+                elif keys[pygame.K_RIGHT]:
+                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, xvel=250 * power))
+
+                elif keys[pygame.K_UP]:
+                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=-250 * power))
+
+                elif keys[pygame.K_LEFT]:
+                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=-250 * power))
+
+                elif keys[pygame.K_DOWN]:
+                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=250 * power))
+
+                elif keys[pygame.K_1]:
+                    power = 1
+                elif keys[pygame.K_2]:
+                    power = 2
+                elif keys[pygame.K_3]:
+                    power = 3
+                elif keys[pygame.K_4]:
+                    power = 4
+                elif keys[pygame.K_5]:
+                    power = 5
+                elif keys[pygame.K_6]:
+                    power = 6
+                elif keys[pygame.K_7]:
+                    power = 7
+                elif keys[pygame.K_8]:
+                    power = 8
+                elif keys[pygame.K_9]:
+                    power = 9
+                elif keys[pygame.K_0]:
+                    power = 0
 
         for ball in balls:
             ball.render()
