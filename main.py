cabde895508b93ced77da28a671a84cd67afb22c
Negolng <mahetov.pavel@gmail.com>
2023-12-05 13:45:21 +0500

    Changed structure, added new classes, added gravity

--- main.py
+++ main.py
@@ -1,213 +1,72 @@
-from random import randint
-from math import sqrt
 import pygame
+from Gravitron import Display, BlackHole, Ball, Gravitron
 
-
-class GravityAbleObject:
-    def __init__(self, disp, cords, xvel=0, yvel=0, grav=9.806):
-        self.disp, self.dispsize, self.fps = disp
-        self.x, self.y = cords
-        self.xvel, self.yvel = xvel, yvel
-        self.gravity_c = grav
-
-    def gravity(self):
-        self.yvel += self.gravity_c
-
-
-class Ball(GravityAbleObject):
-    def __init__(self, disp, cords, dispsize, fps, other_balls, rad=15, colour=None,
-                 kk=0.8, yvel=0, xvel=0, gravity=9.806, do_collide=False):
-        super().__init__((disp, dispsize, fps), cords, xvel=xvel, yvel=yvel, grav=gravity)
-        self.rad = rad
-        self.colour = colour
-        self.balls = other_balls
-        self.kk = kk
-        self.colls = list()
-        self.lx = 0
-        self.ly = 0
-        self.dodraw = True
-        self.do_collide = do_collide
-
-        if not colour:
-            self.colour = randint(0, 255), randint(0, 255), randint(0, 255)
-        else:
-            self.colour = colour
-
-    def draw(self):
-        if self.dodraw:
-            pygame.draw.circle(surface=self.disp, color=self.colour, center=(self.x, self.y), radius=self.rad)
-        else:
-            del self
-
-    def render(self):
-        if self.do_collide:
-            self.other_balls_collide()
-        self.change_cords()
-        self.draw()
-
-    def bounce(self):
-        self.yvel = -(self.yvel * self.kk)
-        self.xvel = self.xvel * self.kk
-
-    def bounce_wall(self):
-        self.yvel = self.yvel * self.kk
-        self.xvel = -(self.xvel * self.kk)
-
-    def collided(self, cords, er=0):
-        outl = []
-        for nut in self.balls:
-            # grab your left nut, make right one jealous!
-            if nut != self:
-                ox, oy = nut.x, nut.y
-                sx, sy = cords
-                xdiff = abs(ox - sx)
-                ydiff = abs(oy - sy)
-                distance = sqrt(xdiff**2 + ydiff**2)
-                if distance + er <= (self.rad + nut.rad):
-                    outl.append(nut)
-        self.colls = outl
-
-    def other_balls_collide(self):
-        self.collided((self.x, self.y))
-        collided_balls = self.colls
-        if collided_balls:
-            for circle in collided_balls:
-                if self.lx == 0 and self.ly == 0:
-                    self.dodraw = False
-
-                olvx = self.xvel
-                olvy = self.yvel
-
-                self.xvel = (-self.xvel * self.kk / 2)
-                self.yvel = (-self.yvel * self.kk / 2)
-
-                circle.xvel += olvx * circle.kk / 2
-                circle.yvel += olvy * circle.kk / 2
-
-                self.x = self.lx
-                self.y = self.ly
-
-    def change_cords(self):
-
-        if self.y + self.rad <= self.dispsize[1]:
-            self.gravity()
-
-        else:
-            if self.yvel > 0:
-                self.y = self.dispsize[1] - self.rad
-                self.bounce()
-
-        newy = self.y + self.yvel / self.fps
-        newx = self.x + self.xvel / self.fps
-
-        self.collided((newx, newy))
-
-        if self.xvel != 0 or self.yvel != 0:
-
-            if newy + self.rad <= self.dispsize[1] and newy - self.rad > 0:
-                self.ly = self.y
-                self.y = newy
-
-            else:
-                if not newy + self.rad <= self.dispsize[1]:
-                    self.ly = self.y
-                    self.y = self.dispsize[1] - self.rad
-                    self.bounce()
-                elif newy - self.rad <= 0:
-                    self.ly = self.y
-                    self.y = self.rad
-                    self.bounce()
-
-            if newx + self.rad <= self.dispsize[0] and newx - self.rad > 0:
-                self.lx = self.x
-                self.x = newx
-
-            else:
-                if not newx + self.rad <= self.dispsize[0]:
-                    self.lx = self.x
-                    self.x = self.dispsize[0] - self.rad
-                    self.bounce_wall()
-                elif newx - self.rad <= 0:
-                    self.lx = self.x
-                    self.x = self.rad
-                    self.bounce_wall()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     pygame.init()
-
-    size = (600, 600)
-    mscr = pygame.display.set_mode(size)
-
-    run = True
-
-    colf = False
-
-    frps = 300
-
+    screensize = (900, 900)
+    screen = pygame.display.set_mode(screensize)
+    running = True
     clock = pygame.time.Clock()
-
-    balls = []
-
-    power = 3
-
-    while run:
-        mscr.fill((255, 255, 255))
+    fps = 120
+    dispy = Display(screen, screensize, fps, 0.001, 9.802, 0.1, 1)
+    popa = []
+    gravipopa = []
+    throw_power = 3
+    while running:
+        screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-
-            if event.type == pygame.KEYDOWN:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                gravipopa.append(Gravitron(pygame.mouse.get_pos(), 6000, 15, dispy))
+            elif event.type == pygame.KEYDOWN:
                 keys = pygame.key.get_pressed()
-                # print(keys)
                 if keys[pygame.K_SPACE]:
-                    balls = []
-
-                elif keys[pygame.K_LEFT]:
-                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, xvel=-250 * power,
-                                      do_collide=colf))
-
+                    popa = []
+                    gravipopa = []
                 elif keys[pygame.K_RIGHT]:
-                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, xvel=250 * power,
-                                      do_collide=colf))
-
-                elif keys[pygame.K_UP]:
-                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=-250 * power,
-                                      do_collide=colf))
-
+                    popa.append(Ball(dispy, pygame.mouse.get_pos(), 15, fps * throw_power, 0, popa, gravipopa))
                 elif keys[pygame.K_LEFT]:
-                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=-250 * power,
-                                      do_collide=colf))
-
+                    popa.append(Ball(dispy, pygame.mouse.get_pos(), 15, -fps * throw_power, 0, popa, gravipopa))
+                elif keys[pygame.K_UP]:
+                    popa.append(Ball(dispy, pygame.mouse.get_pos(), 15, 0, -fps * throw_power, popa, gravipopa))
                 elif keys[pygame.K_DOWN]:
-                    balls.append(Ball(mscr, pygame.mouse.get_pos(), size, frps, balls, yvel=250 * power,
-                                      do_collide=colf))
+                    popa.append(Ball(dispy, pygame.mouse.get_pos(), 15, 0, fps * throw_power, popa, gravipopa))
 
                 elif keys[pygame.K_1]:
-                    power = 1
+                    throw_power = 1
                 elif keys[pygame.K_2]:
-                    power = 2
+                    throw_power = 2
                 elif keys[pygame.K_3]:
-                    power = 3
+                    throw_power = 3
                 elif keys[pygame.K_4]:
-                    power = 4
+                    throw_power = 4
                 elif keys[pygame.K_5]:
-                    power = 5
+                    throw_power = 5
                 elif keys[pygame.K_6]:
-                    power = 6
+                    throw_power = 6
                 elif keys[pygame.K_7]:
-                    power = 7
+                    throw_power = 7
                 elif keys[pygame.K_8]:
-                    power = 8
+                    throw_power = 8
                 elif keys[pygame.K_9]:
-                    power = 9
+                    throw_power = 9
                 elif keys[pygame.K_0]:
-                    power = 0
-                elif keys[pygame.K_F1]:
-                    colf = not colf
+                    throw_power = 0
 
-        for ball in balls:
-            ball.render()
+                elif keys[pygame.K_w]:
+                    dispy.gravity_direction = 3
+                elif keys[pygame.K_a]:
+                    dispy.gravity_direction = 2
+                elif keys[pygame.K_s]:
+                    dispy.gravity_direction = 1
+                elif keys[pygame.K_d]:
+                    dispy.gravity_direction = 4
+
+        for pipa in popa:
+            pipa.render()
+        for gravipipa in gravipopa:
+            gravipipa.render()
 
         pygame.display.flip()
-        clock.tick(frps)
+        clock.tick(fps)
